6e450fdf8edfb37c0d5a7ea4b7d5ef4a691f8749
Rafael Nascimento <rafael.93rj@gmail.com>
2023-04-01 10:26:23 -0300

    new js

--- script.js
+++ script.js
@@ -2,27 +2,22 @@ let button = document.getElementById('botao');
 let inputSal = document.getElementById('salarioLiq');
 let inputDes = document.getElementById('primeiroValor');
 
-let respostaNeg = document.getElementById('respostaDois');
-
-
 function respostaFinal() {
     
-    let salarioLiq = parseFloat(inputSal.value);
-    let primeiroValor = parseFloat(inputDes.value);
+    let entrada = parseFloat(inputSal.value);
+    let saida = parseFloat(inputDes.value);
 
-    let total = salarioLiq - primeiroValor;
-    let porcentagemFixa = Math.round((primeiroValor * 100) / salarioLiq);
+    let total = entrada - saida;
+    let porcentagemFixa = Math.round((saida * 100) / entrada);
 
-    if (salarioLiq > primeiroValor && porcentagemFixa <= '50') {
+    if (entrada > saida && porcentagemFixa <= '50') {
         
         document.getElementById('respostaUm').innerHTML = `<p>Parabéns, seu saldo está positivo, totalizando ${total} reais. <br>Você teve um consumo fixo que corresponde a ${porcentagemFixa}% do seu salário e um consumo variável de xxx,xx</p>`;
         
-        
     } else {
         
-        respostaNeg.innerHTML = `Infelizmente seu saldo é negativo. Precisa de ajuda financeira?`;
+        document.getElementById('respostaDois').innerHTML = `<p>Infelizmente seu saldo é negativo. <br>Precisa de ajuda financeira?</p>`;
         
-        button.parentNode.insertBefore(div, inputSal.nextElementSibling);
     }
 }
 
